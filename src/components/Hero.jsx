9021b7f6f604b0ea83dbe50d8f3ba27cca84d3c9
acquah-forever <kojoaddo598@gmail.com>
2026-05-04 21:03:59 +0000

    git commity

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
                 <div className='flex justify-center -items-center'>
                     <button className='flex justify-center items-center space-x-1 border px-5 py-2 rounded-full bg-emerald-300/25'>
                         <div className='rounded-full border-5 border-emerald-300'></div>
-                        <p className='text-emerald-300 font-semibold'>5000+ jobs available</p></button>
+                        <p className='text-emerald-300 font-semibold'>2000+ jobs available</p></button>
                 </div>
                 <motion.h1 variants={children} className='text-center text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold'>Finding Your Dream <br /> <span className='bg-linear-to-tr from-emerald-400  to-sky-400 text-transparent bg-clip-text'>Software Engineering</span><br /><span>Role Today</span></motion.h1>
                 <motion.h3 variants={children} className='text-center text-xl sm:text-2xl md:text-3xl'>Search through the latest opportunities in software engineering. <br /> Get hired fast with positions updated daily</motion.h3>
