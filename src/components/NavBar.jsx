376ba23bd019a2c4c9aeec99735dd5351e2fba05
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-05-18 19:15:31 +0000

    finalised navbar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,10 +30,10 @@ const NavBar = () => {
         </motion.div>
 
         <div className='text-md md:text-lg hidden md:flex space-x-5'>
-          <Link className='transition-all hover:scale-125 ' to='./'>Home</Link>
-          <Link className='transition-all hover:scale-125 ' smooth to='/search'>Jobs</Link>
+          <Link className='transition-all hover:scale-125 hover:underline duration-300' to='./'>Home</Link>
+          <Link className='transition-all hover:scale-125 hover:underline duration-300' smooth to='/search'>Jobs</Link>
 
-          <button className='cursor-pointer flex justify-center items-center transition-all hover:scale-125 '
+          <button className='cursor-pointer flex justify-center items-center transition-all hover:scale-125 hover:underline duration-300'
             onClick={toggle}>
             More <ChevronDown className='mt-1' size={18} /></button>
 
@@ -42,11 +42,11 @@ const NavBar = () => {
         {user ? (<button className='bg-slate-800 border px-4 py-2 text-lg md:text-xl' onClick={logOut}>LogOut</button>) : (
 
           <div className='hidden md:flex justify-center items-center space-x-3'>
-            <div className=' text-md md:text-lg'>
+            <div className=' text-md md:text-lg transition-all hover:scale-105 bg-linear-to-br text-white from-sky-600 to-slate-800 rounded-xl border px-4 py-2'>
               <NavLink to='./signup'>Sign Up</NavLink>
             </div>
 
-            <div className='bg-linear-to-br text-black from-emerald-400 font-semibold to-green-400 rounded-xl border px-4 py-2 text-md md:text-lg'>
+            <div className='bg-linear-to-br text-black from-emerald-400 font-semibold to-green-400 rounded-xl border px-4 py-2 text-md md:text-lg transition-all hover:scale-105'>
               <NavLink to='./login'>Log In</NavLink>
             </div>
           </div>
